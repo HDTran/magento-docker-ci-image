b8197e701e7e7c6a59dbb8a9485e6dd0851a3b82
Bram van de Sande <bram@deity.io>
2018-11-08 22:47:08 +0100

    remove useless bash from the up command

--- up.sh
+++ up.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-docker run -p 80:80 -p 3306:3306  -it  --name="deity-magento2-ci" deity-magento2-ci /bin/bash
+docker run -p 80:80 -p 3306:3306  -it  --name="deity-magento2-ci" deity-magento2-ci
